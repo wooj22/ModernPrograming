83219297db010f675eb1fc1c5048f4631e645323
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-09-18 17:03:21 +0900

    0918 | RAII, 스마트포인터

--- ModernC++/ModernC++/main.cpp
+++ ModernC++/ModernC++/main.cpp
@@ -2,23 +2,33 @@
 #include <vector>
 using namespace std;
 
-class A {};
-
-void foo(A& a) { std::cout << "A& a" << std::endl; }
-void foo(const A& a) { std::cout << "const A& a" << std::endl; }
-void foo(A&& a) { std::cout << "A&& a" << std::endl; }
-
-template <typename T>
-void wrapper(T&& u) 
+class Player
 {
-	foo(std::forward<T>(u));
-}
+	string name;
 
-int main() {
-	A a;
-	const A ca;
+public:
+	Player() : name("") {}
+	Player(string n) : name(n) {}
+	void Show() { cout << name << endl; }
+};
 
-	wrapper(a);
-	wrapper(ca);
-	wrapper(A());
+int main() 
+{
+	vector<unique_ptr<Player>> v;
+	v.push_back(make_unique<Player>("1"));
+	v.push_back(make_unique<Player>("2"));
+	v.push_back(make_unique<Player>("3"));
+
+	v[0]->Show();
+	v[1]->Show();
+	v[2]->Show();
+
+	vector<unique_ptr<Player>> v2(3);
+	v2[0] = make_unique<Player>("1");
+	v2[1] = make_unique<Player>("2");
+	v2[2] = make_unique<Player>("3");
+
+	v2[0]->Show();
+	v2[1]->Show();
+	v2[2]->Show();
 }
