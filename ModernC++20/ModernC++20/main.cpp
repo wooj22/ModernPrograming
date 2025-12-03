6aac7a92c9624fbc8eee3524d9fe970765934cc8
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-12-03 16:51:35 +0900

    update

--- ModernC++20/ModernC++20/main.cpp
+++ ModernC++20/ModernC++20/main.cpp
@@ -1,6 +1,2 @@
 #include<iostream>
 using namespace std;
-
-struct [[deprecated("this struct is outdated")]] OldStruct { int a; };
-[[nodiscard]] int getValue() { return 42; }
-[[maybe_unused]] int unusedVariable = 10;
